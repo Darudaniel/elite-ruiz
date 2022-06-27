9ca2f9afc9dfe5529a59bea73c8af1a948d465e8
Daru <danielelrincon@gmail.com>
2022-06-27 11:45:29 -0500

    whatsapp button added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,19 @@ import '../styles/components/Layout.css'
 import Header from './Header'
 import Footer from './Footer'
 import Banner from '../assets/images/EliteRuizBanner.png'
+import { Icon } from '@iconify/react';
 
 const Layout = ({ children }) => {
+  const whatsapp = <Icon icon="logos:whatsapp" style={{ fontSize: '5.5rem'}}/>
+
   return (
     <div className="Layout">
       <Header image={Banner} />
       <div className="layout-content-container">
         {children}
+        <div className="whatsapp-container">
+          {whatsapp}
+        </div>
       </div>
       <Footer />
     </div>    
