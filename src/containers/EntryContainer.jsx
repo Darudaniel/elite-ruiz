5ac43bb595ce029c93fca4354940be22a5b2cae2
darudaniel <habladaru@gmail.com>
2024-01-18 17:19:35 -0500

    react helmet

--- src/containers/EntryContainer.jsx
+++ src/containers/EntryContainer.jsx
@@ -5,7 +5,7 @@ import { db, registerEvent } from '../firebase';
 import '../styles/containers/EntryContainer.css'
 import EntriesList from '../components/EntriesList';
 import ReactMarkdown from 'react-markdown';
-
+import { Helmet } from 'react-helmet';
 
 const EntryContainer = () => {
   const { entryId } = useParams()
@@ -23,29 +23,40 @@ const EntryContainer = () => {
   }, [entryId])
 
     return(
-      <div className='EntryContainer'>
-        {myEntry.title ?
-          <div className='my-entry'>
-            <h1 className='entry-title'>{myEntry.title}</h1>
-            <img className="entry-img" src={myEntry.img} alt="foto relacionada con el articulo" />
-            <p className='entry-opening'>{myEntry.opening}</p>
-            <div className='markdown-container'>
-              <ReactMarkdown>{myEntry.editorContent}</ReactMarkdown>
-            </div>
-            {myEntry.author ? 
-                <p className='entry-opening'>Escrito por: {myEntry.author}</p>
-               :
-                <p className='entry-opening'>Escrito por: Anónimo</p>
-            }
-          </div>
-          : 
-          console.log('Aun no ha llegado la respuesta de la api')
-        }
-        <h3 className="articles-section-title">Mas artículos</h3>
-        <EntriesList />
-      </div>
-    )
-  
-}
-
+      
+       
+        <div className='EntryContainer'>
+          {myEntry.title ?
+            <>
+              <Helmet>
+                <title>{myEntry.title}</title>
+                <meta name="description" content={myEntry.opening} />
+                <meta property="og:title" content={myEntry.title} />
+                <meta property="og:description" content={myEntry.opening} />
+                <meta property="og:image" content={myEntry.img} />
+              </Helmet>
+              <div className='my-entry'>
+                <h1 className='entry-title'>{myEntry.title}</h1>
+                <img className="entry-img" src={myEntry.img} alt="foto relacionada con el articulo" />
+                <p className='entry-opening'>{myEntry.opening}</p>
+                <div className='markdown-container'>
+                  <ReactMarkdown>{myEntry.editorContent}</ReactMarkdown>
+                </div>
+                {myEntry.author ? 
+                    <p className='entry-opening'>Escrito por: {myEntry.author}</p>
+                    :
+                    <p className='entry-opening'>Escrito por: Anónimo</p>
+                  }
+              </div>
+            </>
+            : 
+            console.log('Aun no ha llegado la respuesta de la api')
+          }
+          <h3 className="articles-section-title">Mas artículos</h3>
+          <EntriesList />
+        </div>
+      
+      )
+    }
+    
 export default EntryContainer
